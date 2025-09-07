d8072afc37a10d5a950eeae8fa9113b228bec738
Ekomjah Denis <ekomjahedet@gmail.com>
2025-09-07 23:24:41 +0100

    Add file-loader and image-webpack-loader for image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,16 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        loader: "file-loader",
+      },
+      {
+        loader: "image-webpack-loader",
+        options: {
+          mozjpeg: { progressive: true, quality: 65 },
+          pngquant: { quality: [0.65, 0.9], speed: 4 },
+        },
+      },
     ],
   },
 };
